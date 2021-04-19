9a86dcb0dc72db9260b0b1f4f2d84dec082f3a31
Adam Dilek <dilekmuhammet@gmail.com>
2021-04-19 20:14:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,32 +6,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-
-	log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(os.Stdout)
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		f := fib()
 
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, "Hello World From Sunny California - master!\n")
-
-// 		for _, e := range os.Environ() {
-// 			pair := strings.Split(e, "=")
-// 			io.WriteString(w, pair[0]+"="+pair[1]+"\n")
-// 		}
-
-		for i := 1; i <= 90; i++ {
-			io.WriteString(w, strconv.Itoa(f())+"\n")
-		}
-
-		log.Info("Hello world called - this is the log message")
 
 	})
 	http.ListenAndServe(":80", nil)
